a7ab292824b28c6687bbbe2529ce9ce5dd495e6b
Henry <mail@henrygressmann.de>
2021-10-23 00:17:15 +0200

    release based on commit hash
    Signed-off-by: Henry <mail@henrygressmann.de>

--- typescript/scripts/bump.js
+++ typescript/scripts/bump.js
@@ -1,9 +1,12 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const pkg = JSON.parse(readFileSync("./package.json"));
+import shell from "shelljs";
 
+const hash = shell.exec("git show -s --format=%h").trim();
+
+const pkg = JSON.parse(readFileSync("./package.json"));
 let version = pkg.version.split(".").map(i => parseInt(i));
-version[version.length - 1] += 1;
+version[version.length - 1] = `0-${hash}`;
 pkg.version = version.join(".");
 
 writeFileSync("./package.json", JSON.stringify(pkg, null, 2), "utf-8");
